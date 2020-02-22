0d864ad6cd5f49a7737c82945bafba192bcf11ec
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-02-23 07:24:32 +0900

    add: shell script for linux

---
+++ js/getlibs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/mrdoob/three.js/dev/build/three.min.js -o three.min.js
+wget https://raw.githubusercontent.com/kripken/ammo.js/master/builds/ammo.js -o ammo.js
