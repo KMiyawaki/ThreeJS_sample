e20f5a1a45ac9dc56a936adc3d9fe5eda643cd0c
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-02-25 07:24:20 +0900

    remove: local JavaScript local download

--- js/getlibs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/mrdoob/three.js/dev/build/three.min.js -O three.min.js
-wget https://raw.githubusercontent.com/kripken/ammo.js/master/builds/ammo.js -O ammo.js
