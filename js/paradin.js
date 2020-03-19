f48e2da06f10b509c77a4addaa062363b62b6df6
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2020-03-19 15:56:37 +0900

    add: attack animation by push button

--- js/paradin.js
+++ js/paradin.js
@@ -1,16 +1,31 @@
 const Paradin = {
     onUpdate: function (ammo, character, deltaTime, params) {
         let nextAction = character.userData.crntAction;
-        if (Math.abs(character.userData.linearVelocity) > 0 || Math.abs(character.userData.angularVelocity) > 0) {
-            nextAction = character.userData.actions[1];
-        } else {
-            nextAction = character.userData.actions[0];
+        if (character.userData.crntAction == character.userData.actions[0]) {
+            if (character.userData.attack) {
+                nextAction = character.userData.actions[2];
+            } else if (Math.abs(character.userData.linearVelocity) > 0 || Math.abs(character.userData.angularVelocity) > 0) {
+                nextAction = character.userData.actions[1];
+            }
+        } else if (character.userData.crntAction == character.userData.actions[1]) {
+            if (character.userData.attack) {
+                nextAction = character.userData.actions[2];
+                character.userData.attack = false;
+            } else if (Math.abs(character.userData.linearVelocity) <= 0 && Math.abs(character.userData.angularVelocity) <= 0) {
+                nextAction = character.userData.actions[0];
+            }
+        } else if (character.userData.crntAction == character.userData.actions[2]) {
+            if (character.userData.crntAction.time + deltaTime >= character.userData.crntAction.getClip().duration) {
+                nextAction = character.userData.actions[0];
+                character.userData.attack = false;
+            }
         }
         if (nextAction != character.userData.crntAction) {
             character.userData.crntAction.stop();
             character.userData.crntAction = nextAction;
             character.userData.crntAction.play();
         }
+        console.log(character.userData.crntAction.time + "/" + character.userData.crntAction.getClip().duration);
         character.userData.mixer.update(deltaTime);
     }
 };
